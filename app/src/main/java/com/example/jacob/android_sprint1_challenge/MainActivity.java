c1dfb0315017e83f125474f3218449de89a926db
Jacob Seppanen <jbseppanen@gmail.com>
2018-11-09 09:50:50 -0800

    Basic app with working main screen. **Add movie button crashes.**

--- app/src/main/java/com/example/jacob/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/jacob/android_sprint1_challenge/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.jacob.android_sprint1_challenge;
 
+import android.app.Activity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Context context;
     private LinearLayout layoutList;
-    private NoteViewModel viewModel;
+    private MovieViewModel viewModel;
 
     public static final int EDIT_REQUEST_CODE = 1;
 
@@ -21,5 +30,80 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         layoutList = findViewById(R.id.layout_list);
 
+        viewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
+
+        final Observer<ArrayList<Movie>> observer = new Observer<ArrayList<Movie>>() {
+            @Override
+            public void onChanged(@Nullable ArrayList<Movie> movies) {
+                if (movies != null) {
+                    refreshListView(movies);
+                }
+
+            }
+        };
+        viewModel.getMovieList().observe(this, observer);
+
+        findViewById(R.id.button_addmovie).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, EditActivity.class);
+                Movie newMovie = new Movie(Movie.NO_ID);
+
+                intent.putExtra(EditActivity.EDIT_MOVIE_KEY, newMovie);
+                startActivityForResult(intent, EDIT_REQUEST_CODE);
+            }
+        });
+    }
+
+    private TextView getDefaultTextView(final Movie movie) {
+        TextView textView = new TextView(context);
+        textView.setText(movie.getTitle());
+        textView.setTextSize(24);
+        textView.setPadding(10, 10, 10, 10);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, EditActivity.class);
+                intent.putExtra(EditActivity.EDIT_MOVIE_KEY, movie);
+                startActivityForResult(intent, EDIT_REQUEST_CODE);
+            }
+        });
+
+        return textView;
+    }
+
+    private void refreshListView( ArrayList<Movie> movies) {
+        layoutList.removeAllViews();
+        for (Movie movie : movies) {
+            layoutList.addView(getDefaultTextView(movie));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == EDIT_REQUEST_CODE) {
+                if (data != null) {
+                    Movie returnedMovie = (Movie) data.getSerializableExtra(EditActivity.EDIT_MOVIE_KEY);
+
+/*                    boolean foundMovie = false;
+                    for(int i = 0; i < movies.size(); ++i) {
+                        if(movies.get(i).getId() == returnedMovie.getId()) {
+                            // this created a bug with an infinite loop, with each loop,
+                            // an element is inserted into the beginning of the arraylist
+//                            movies.add(i, returnedMovie);
+                            movies.set(i, returnedMovie);
+                            foundMovie = true;
+                        }
+                    }
+                    if(!foundMovie) {
+                        movies.add(returnedMovie);
+                    }
+                    refreshListView();*/
+                    viewModel.addMovie(returnedMovie);
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/jacob/android_sprint1_challenge/MovieViewModel.java
+++ app/src/main/java/com/example/jacob/android_sprint1_challenge/MovieViewModel.java
@@ -1,4 +1,34 @@
 package com.example.jacob.android_sprint1_challenge;
 
-public class MovieViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class MovieViewModel extends ViewModel {
+
+    private MutableLiveData<ArrayList<Movie>> movieList;
+    private MovieRepository repo;
+
+    public LiveData<ArrayList<Movie>> getMovieList() {
+        if (movieList == null) {
+            loadList();
+
+        }
+        return movieList;
+    }
+
+
+    private void loadList() {
+        repo = new MovieRepository();
+        movieList = repo.getMovies();
+    }
+
+    public void addMovie(Movie movie) {
+        if(movieList != null) {
+            movieList.setValue(repo.addMovie(movie));
+        }
+    }
+
 }
