aa0121c4fd842aa841cdf11ef39a8897de7d3a9a
Patrick Martin <patrickmartin88@gmail.com>
2018-11-09 17:04:53 -0800

    adding movie java classes.

--- app/src/main/java/com/example/patrickjmartin/android_sprint1_challenge/Movie.java
+++ app/src/main/java/com/example/patrickjmartin/android_sprint1_challenge/Movie.java
@@ -1,4 +1,42 @@
 package com.example.patrickjmartin.android_sprint1_challenge;
 
-public class Movie {
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+
+    public static final int NO_ID = -1;
+
+    private String title;
+    private boolean watched;
+    private int id;
+
+    public Movie(String title, boolean watched, int id) {
+        this.title = title;
+        this.watched = watched;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- app/src/main/java/com/example/patrickjmartin/android_sprint1_challenge/MovieRepo.java
+++ app/src/main/java/com/example/patrickjmartin/android_sprint1_challenge/MovieRepo.java
@@ -1,4 +1,27 @@
 package com.example.patrickjmartin.android_sprint1_challenge;
 
+import android.arch.lifecycle.MutableLiveData;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MovieRepo {
+
+    private ArrayList movies;
+
+    public MovieRepo(){
+        this.movies = new ArrayList<>();
+    }
+
+    public MutableLiveData<ArrayList<Movie>> getMovies(){
+        MutableLiveData<ArrayList<Movie>> liveDataList = new MutableLiveData<>();
+        liveDataList.setValue(movies);
+        return liveDataList;
+    }
+
+    public ArrayList<Movie> addMovie(Movie movie) {
+        if(movie.getId() == Movie.NO_ID){
+
+        }
+    }
 }
