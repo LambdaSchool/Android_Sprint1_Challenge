06a1be2ab07fc384ad4d07b230f1f3dbf5290f89
Patrick Martin <patrickmartin88@gmail.com>
2018-11-15 15:46:19 -0800

    completed the mainActivity.

--- app/src/main/java/com/example/patrickjmartin/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/patrickjmartin/android_sprint1_challenge/MainActivity.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -47,36 +48,54 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, AddDetails.class);
+                Intent addMovieIntent = new Intent(context, AddDetails.class);
                 Movie movie = null;
-                intent.putExtra(AddDetails.EDIT_MOVIE_KEY, movie);
-                startActivityForResult(intent, EDIT_MOVIE_REQUEST_CODE);
+                addMovieIntent.putExtra(AddDetails.EDIT_MOVIE_KEY, movie);
+                startActivityForResult(addMovieIntent, EDIT_MOVIE_REQUEST_CODE);
             }
         });
     }
 
-    private TextView getDefaultTextView(MovieOverview movie) {
-        TextView view = new TextView(context);
-        final String releaseYear = movie.getRelease_date().split("-")[0];
-        String displayText = String.format("%s (%s)", movie.getTitle(), releaseYear);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if(requestCode == EDIT_MOVIE_REQUEST_CODE ) {
+                if(data != null) {
+                    Movie retMovie = (Movie) data.getSerializableExtra(AddDetails.EDIT_MOVIE_KEY);
+                    viewModel.addMovie(retMovie);
+                }
+            }
+        }
+    }
 
-        view.setText(displayText);
-        view.setTextSize(28);
-        view.setOnClickListener(new View.OnClickListener() {
+    private TextView getDefaultTextView(final Movie movie) {
+        TextView textView = new TextView(context);
+        textView.setText(movie.getTitle());
+        boolean watched = movie.isWatched();
+
+        if (watched) {
+            textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+
+        textView.setTextSize(24);
+        textView.setPadding(10, 10, 10, 10);
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //int id = movie.getId();
-                //Log.i("MovieElement", Integer.toString(id));
+                viewModel.removeMovie(movie);
+                Intent editMovieIntent = new Intent(context, AddDetails.class);
+                editMovieIntent.putExtra(AddDetails.EDIT_MOVIE_KEY, movie);
+
             }
         });
-
-        return view;
+        return textView;
     }
 
     private void refreshListView(ArrayList<Movie> movies) {
         layoutList.removeAllViews();
         for (Movie movie : movies) {
-            //layoutList.addView(getDefaultTextView(movie));
+            layoutList.addView(getDefaultTextView(movie));
         }
     }
 }
