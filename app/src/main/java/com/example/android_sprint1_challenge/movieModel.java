738d4e010004c5ecd13fc7bca58c3f93b527362e
gft-it-man <gftitman@gmail.com>
2019-03-22 11:18:05 -0500

    built movie model class

--- app/src/main/java/com/example/android_sprint1_challenge/movieModel.java
+++ app/src/main/java/com/example/android_sprint1_challenge/movieModel.java
@@ -1,5 +1,39 @@
 package com.example.android_sprint1_challenge;
 
-public class movieModel {
+public class movieModel
+{
+    int index;
+    String title;
+    boolean seen;
 
+    public movieModel(int index, String title, boolean seen)
+    {
+        this.index = index;
+        this.title = title;
+        this.seen = seen;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
+    }
 }
