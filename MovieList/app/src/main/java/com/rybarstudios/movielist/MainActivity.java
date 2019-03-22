ef66f1ecb1c50671a7ad8f86223b30586356960a
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-03-22 11:26:31 -0400

    Created TetView generator in Main Activity

--- MovieList/app/src/main/java/com/rybarstudios/movielist/MainActivity.java
+++ MovieList/app/src/main/java/com/rybarstudios/movielist/MainActivity.java
@@ -1,14 +1,33 @@
 package com.rybarstudios.movielist;
 
+import android.content.Context;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
+    }
+
+    private TextView genTextView(Movie movie) {
+        TextView view = new TextView(context);
+        view.setText(movie.getTitle());
+        view.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+
+        if(movie.isWatched()) {
+            view.setPaintFlags(view.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+
+        return view;
     }
 }
