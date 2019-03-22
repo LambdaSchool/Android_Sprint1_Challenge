90db9d5f78b8e5e49e2a9b5ba3dddec14dbe0f9f
BL-CoderOvertime <brandonlively@live.com>
2019-03-22 12:23:40 -0400

    added CreateTextView and OnClickListeners for MovieList

--- app/src/main/java/com/example/movielist/AddMovieActivity.java
+++ app/src/main/java/com/example/movielist/AddMovieActivity.java
@@ -1,5 +1,6 @@
 package com.example.movielist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class AddMovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_movie);
+        Intent intent = this.getIntent();
     }
 }
--- app/src/main/java/com/example/movielist/MovieListActivity.java
+++ app/src/main/java/com/example/movielist/MovieListActivity.java
@@ -1,13 +1,49 @@
 package com.example.movielist;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MovieListActivity extends AppCompatActivity {
+
+    Button buttonAdd;
+    LinearLayout ll;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        buttonAdd = findViewById(R.id.add_button);
+        ll = findViewById(R.id.ll_scrollview);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, AddMovieActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public TextView createTextView(MovieEntry entry){
+        TextView tv = new TextView(context);
+        tv.setText(entry.getTitle());
+        tv.setPadding(15,7,15,7);
+        tv.setTextSize(24);
+        if(entry.isWatched()){
+            tv.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+
+        return tv;
     }
 }
