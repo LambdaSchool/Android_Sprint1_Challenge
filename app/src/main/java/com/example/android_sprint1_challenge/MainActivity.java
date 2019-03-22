f3c34c371f09c337b18ef59fb50d804d71f136d4
Janiece Thomas <janiece.t90@gmail.com>
2019-03-22 19:24:16 -0400

    Added new Activity ClickAddMovieButton

--- app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android_sprint1_challenge;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 class MainActivity extends AppCompatActivity {
@@ -19,11 +21,18 @@ class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = getIntent();
-        entry = (MovieEntry) intent.getSerializableExtra(MovieEntry.TAG);
-        if(entry == null) {
-            entry = new MovieEntry(MovieEntry.INVALID_ID);
-        }
+        final Button button = findViewById(R.id.add_movie);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                entry = (MovieEntry) intent.getSerializableExtra(MovieEntry.TAG);
+                if(entry == null) {
+                    entry = new MovieEntry(MovieEntry.INVALID_ID);
+                }
+            }
+        });
+
+
 
 
     }
