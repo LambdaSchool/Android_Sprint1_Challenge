63b6cb7598bb41d487cc16fbae7fde03d10701ac
Janiece Thomas <janiece.t90@gmail.com>
2019-03-22 17:44:42 -0400

    Created condector,getters, setters fot Movie Entry class

--- app/src/main/java/com/example/android_sprint1_challenge/MovieEntry.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MovieEntry.java
@@ -1,4 +1,26 @@
 package com.example.android_sprint1_challenge;
 
+import java.io.Serializable;
+
 public class MovieEntry implements Serializable {
+
+    private String movieName;
+
+    public MovieEntry(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public MovieEntry(int id){
+        this.movieName = "";
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
 }
+
+
--- app/src/main/java/com/example/android_sprint1_challenge/MovieListActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MovieListActivity.java
@@ -1,4 +1,6 @@
 package com.example.android_sprint1_challenge;
 
 public class MovieListActivity implements Serializable {
+
+
 }
