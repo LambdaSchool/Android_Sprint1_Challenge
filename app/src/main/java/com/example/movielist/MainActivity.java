1975f9daedda29570d98207810d60c8c4577cab0
Spencer Stock <spencerstock@gmail.com>
2019-03-22 09:50:16 -0600

    functioning textview generator

--- app/src/main/java/com/example/movielist/MainActivity.java
+++ app/src/main/java/com/example/movielist/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.movielist;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,8 +20,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context = this;
         setContentView(R.layout.activity_main);
+        context = this;
+
+        Intent intent = getIntent();
+        if ((ArrayList)intent.getSerializableExtra("movieList") != null) {
+            movieList = (ArrayList) intent.getSerializableExtra("movieList");
+        }
+
+        for (int i = 0; i < movieList.size(); ++i){
+            MovieData temp = movieList.get(i);
+            LinearLayout linearLayout = findViewById(R.id.linear_layout_parent);
+            linearLayout.addView(textViewGenerator(temp.getTitle(),temp.isWatched()));
+        }
 
         Button addMovie = findViewById(R.id.button_add_movie);
         addMovie.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +45,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private TextView textViewGenerator(String title, boolean watched) {
+        TextView tv = new TextView(context);
+        tv.setText(title);
+        if (!watched) tv.setTypeface(Typeface.DEFAULT_BOLD);
+        return tv;
     }
 }
