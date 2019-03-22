049d64e789c345b7982a021072055298f878836b
Janiece Thomas <janiece.t90@gmail.com>
2019-03-22 12:53:00 -0400

    Made onclick listener for buttom

--- app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.android_sprint1_challenge;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import static android.content.Intent.ACTION_SEND;
+
+class MovieActivity extends AppCompatActivity {
 
 
-
+Context context;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        ((Button)findViewById(R.id.add_movie)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addMovieButton = new Intent(ACTION_SEND);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android_sprint1_challenge/MovieEntryActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.android_sprint1_challenge;
-
-public class MovieEntryActivity {
-
-
-}
