69d1bc357f4322fb6b009d9ba84f33b9ed585779
gft-it-man <gftitman@gmail.com>
2019-03-22 10:45:55 -0500

    add button and build initial view

--- app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.android_sprint1_challenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.add_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+
+
+            }
+        });
+
+
+
     }
 }
