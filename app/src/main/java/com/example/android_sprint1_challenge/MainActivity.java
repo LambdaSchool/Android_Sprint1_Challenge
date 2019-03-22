7881967ea1f7ac21ce7f3cf9366c75a278382350
Kyle Cole <kyle@misfitselectronics.com>
2019-03-22 19:13:12 -0400

    trying to figure out where the repo isnt accessing to create a new textview

--- app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
@@ -2,65 +2,56 @@ package com.example.android_sprint1_challenge;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    LinearLayout layoutLinear;
+public class MainActivity extends AppCompatActivity implements Serializable {
+    LinearLayout layoutLinear ;
     Intent fullIntent;
     Button addButton;
     Context context;
-    Boolean viewed;
     ArrayList movieArray = new ArrayList<String>();
+    MovieObject newMovie = new MovieObject();
+    public int i;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent saveMovie =getIntent();
-        MovieStatus newMovie = (MovieStatus) saveMovie.getSerializableExtra("newmovie");
-        String movieName = newMovie.getMovieName();
-        Boolean isViewed = newMovie.isViewed();
-
-
-
-        if(movieName != null)
-        {
-            layoutLinear.removeAllViews();
-            int listIndex = movieArray.indexOf(movieName);
-            String storedMovieName = movieArray.get(listIndex).toString();
-
-            MovieStatus movie = new MovieStatus(movieName, isViewed);
-            movieArray.add(movie);
-        }
-        for(int i = 0; i < movieArray.size(); ++i){
-            MovieStatus listedMovie = (MovieStatus) movieArray.get(i);
-            String listedMovieName = listedMovie.getMovieName();
-            Boolean listedMovieWatched = listedMovie.isViewed();
-            createTextView(listedMovieName, i, listedMovieWatched);
-        }
-
-
-
-
-
+       layoutLinear = findViewById(R.id.scroll_view);
         addButton = findViewById(R.id.button_add_movie);
         context = this;
+
+            Intent saveMovie = getIntent();
+                newMovie = (MovieObject) saveMovie.getSerializableExtra("newmovie");
+
+                layoutLinear.removeAllViews();
+
+
+            if(newMovie!=null) {
+                movieArray.add(newMovie);
+                for (i = 0 ; i < movieArray.size(); ++i) {
+                    MovieObject listedMovie = (MovieObject) movieArray.get(i);
+                    String listedMovieName = listedMovie.getMovieName();
+                    Boolean listedMovieWatched = listedMovie.isViewed();
+                    createTextView(listedMovieName, i, listedMovieWatched);
+                }
+            }
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent addMovie = new Intent(context, MovieDeltails.class);
+                Intent addMovie = new Intent(context, MovieDetails.class);
+                int indexValue = i;
+                addMovie.putExtra("index", indexValue);
                 startActivity(addMovie);
 
 
@@ -72,22 +63,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
     public TextView createTextView(final String imageText, final int listIndex, final Boolean isViewed){
-        fullIntent = new Intent(MainActivity.this, MovieDeltails.class);
+        fullIntent = new Intent(MainActivity.this, MovieDetails.class);
         TextView        textView        = new TextView (getApplicationContext ());
-        layoutLinear    = findViewById ( R.id.scroll_view );
         layoutLinear.addView ( textView);
         textView.setId ( listIndex );
         textView.setText ( imageText );
