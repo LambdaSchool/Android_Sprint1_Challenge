f13298814e6348567d2d22c068fce81a717437b6
gft-it-man <gftitman@gmail.com>
2019-03-22 10:55:44 -0500

    build initial edit view and add intent to communicate with new view

--- app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android_sprint1_challenge;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button addButton;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addButton = findViewById(R.id.add_button);
+        context = this;
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-
-
+                Intent  intent = new Intent(context, editList.class);
+                startActivity(intent);
 
             }
         });
