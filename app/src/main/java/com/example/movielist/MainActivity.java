a878a41790f9df57d614a935ab316ef270bd211e
Spencer Stock <spencerstock@gmail.com>
2019-03-22 09:09:51 -0600

    setup arraylist added bones of 2nd activity made button and started calling intent

--- app/src/main/java/com/example/movielist/MainActivity.java
+++ app/src/main/java/com/example/movielist/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.movielist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<MovieData> movieList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addMovie = findViewById(R.id.button_add_movie);
+        addMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("movieList", movieList);
+                startActivity();
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/movielist/MovieData.java
@@ -0,0 +1,36 @@
+package com.example.movielist;
+
+import java.io.Serializable;
+
+public class MovieData implements Serializable {
+    private String title;
+    private boolean watched;
+    private int index;
+
+    public MovieData(String title, boolean watched, int index) {
+        this.title = title;
+        this.watched = watched;
+        this.index = index;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+}
