47aa11eecfceb3fc6db5e1c7f09e7f9db8a75896
Janiece Thomas <janiece.t90@gmail.com>
2019-03-22 16:53:27 -0400

    Had to restatt because A class was inside of a class

--- app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
@@ -1,37 +1,13 @@
 package com.example.android_sprint1_challenge;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import static android.content.Intent.ACTION_SEND;
-
-class MovieActivity extends AppCompatActivity {
-
-
-Context context;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        context = this;
-
-        /*findViewById(R.id.add_movie).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent addMovieButton = new Intent(ACTION_SEND);
-                startActivityForResult(addMovieButton, ??? );
-            }
-        });*/
-
-        LOST!
     }
-
-
 }
