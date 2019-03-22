9ac45ef3ef80c6df0959e246ef850368dbe2a409
BL-CoderOvertime <brandonlively@live.com>
2019-03-22 11:58:04 -0400

    Set up MovieEntryRepo

--- app/src/main/java/com/example/movielist/MovieEntryRepo.java
+++ app/src/main/java/com/example/movielist/MovieEntryRepo.java
@@ -1,4 +1,17 @@
 package com.example.movielist;
 
+import java.util.ArrayList;
+
 public class MovieEntryRepo {
+    public static ArrayList<MovieEntry> movieList = new ArrayList<>();
+
+
+    public MovieEntry getMovieEntry(int id){
+        return movieList.get(id);
+    }
+
+    public void addToMovieList(MovieEntry entry){
+        movieList.add(entry);
+    }
+
 }
