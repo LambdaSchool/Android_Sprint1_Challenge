e894c721cb6e912d737e392ffc666531b3dae3af
Janiece Thomas <janiece.t90@gmail.com>
2019-03-22 17:37:52 -0400

    Added Classes and made them Serializable

--- app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.android_sprint1_challenge;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity extends AppCompatActivity {
+
+
+    Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/android_sprint1_challenge/MovieListActivity.java
@@ -0,0 +1,4 @@
+package com.example.android_sprint1_challenge;
+
+public class MovieListActivity implements Serializable {
+}
