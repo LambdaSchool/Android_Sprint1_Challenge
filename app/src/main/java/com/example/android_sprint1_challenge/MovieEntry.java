8c2ba46904d35f88e42fcb664e23c6232093f882
Janiece Thomas <janiece.t90@gmail.com>
2019-03-22 12:15:07 -0400

    created conductor, getter and setter for movieName in my movieEntry class

--- app/src/main/java/com/example/android_sprint1_challenge/MovieEntry.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MovieEntry.java
@@ -1,4 +1,18 @@
 package com.example.android_sprint1_challenge;
 
 public class MovieEntry {
+
+    public MovieEntry(String movieName) {
+        this.movieName = movieName;
+    }
+
+    private String movieName;
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
 }
