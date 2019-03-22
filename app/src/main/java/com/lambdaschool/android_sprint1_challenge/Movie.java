69af24cf5bf58f36da7d1aef5e3c7e744a3e0ce9
heiligbasil <basilief@yahoo.com>
2019-03-22 10:13:46 -0700

    Hooked up the code in the List Activity

--- app/src/main/java/com/lambdaschool/android_sprint1_challenge/Movie.java
+++ app/src/main/java/com/lambdaschool/android_sprint1_challenge/Movie.java
@@ -8,6 +8,11 @@ public class Movie implements Serializable {
     private String movieTitle;
     private boolean watched;
 
+    public Movie() {
+        this.movieTitle = "";
+        this.watched = false;
+    }
+
     public Movie(String movieTitle, boolean watched) {
         this.movieTitle = movieTitle;
         this.watched = watched;
--- app/src/main/java/com/lambdaschool/android_sprint1_challenge/MovieList.java
+++ app/src/main/java/com/lambdaschool/android_sprint1_challenge/MovieList.java
@@ -1,9 +1,14 @@
 package com.lambdaschool.android_sprint1_challenge;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.StrikethroughSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MovieList extends AppCompatActivity {
 
@@ -12,11 +17,37 @@ public class MovieList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
 
+        Intent intent = getIntent();
+        final Movie movieToAdd = (Movie) intent.getSerializableExtra("movie");
+
+        LinearLayout linearLayout = findViewById(R.id.linear_layout_movies);
+        final TextView addTextView = new TextView(this);
+
+        if (movieToAdd != null) {
+            addTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            addTextView.setText(movieToAdd.getMovieTitle());
+            addTextView.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+            if (movieToAdd.isWatched())
+                addTextView.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            addTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), MovieEdit.class);
+                    intent.putExtra("movie", new Movie(addTextView.getText().toString(), addTextView.getPaint().isStrikeThruText()));
+                    startActivity(intent);
+                }
+            });
+
+            linearLayout.addView(addTextView);
+        }
+
         Button buttonAddMovie = findViewById(R.id.button_add_movie);
         buttonAddMovie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getApplicationContext(), MovieEdit.class);
+                intent.putExtra("movie", 0);
+                startActivity(intent);
             }
         });
     }
