f9a19652367962df7aa416e0d15fc1b85b29c10a
voodoopdx <ourquotesdefineus@gmail.com>
2019-03-22 09:07:41 -0700

    Added Instructions on edit activity

--- app/src/main/java/com/example/watchedmovielist/EditActivity.java
+++ app/src/main/java/com/example/watchedmovielist/EditActivity.java
@@ -1,6 +1,17 @@
 package com.example.watchedmovielist;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class EditActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 1. Check and see if sent movie
+        // 2. if yes, populate fields
+        // 3. Set save/delete functionality via onclick listeners for buttons
+        
+    }
 }
