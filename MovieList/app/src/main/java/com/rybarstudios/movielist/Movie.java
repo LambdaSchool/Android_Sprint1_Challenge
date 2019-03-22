754e520fb02dea1f827549b85ef8ac7f56479b41
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-03-22 11:05:28 -0400

    Created Movie POJO

---
+++ MovieList/app/src/main/java/com/rybarstudios/movielist/Movie.java
@@ -0,0 +1,43 @@
+package com.rybarstudios.movielist;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+    private String title;
+    private boolean watched;
+    private int id;
+
+    public Movie(String title, boolean watched, int id) {
+        this.title = title;
+        this.watched = watched;
+        this.id = id;
+    }
+
+    public Movie(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
