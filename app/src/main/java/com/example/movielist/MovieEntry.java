2b1b2de6971c045965af6c3381f61a4a4208b2d8
BL-CoderOvertime <brandonlively@live.com>
2019-03-22 11:23:51 -0400

    Fleshed out MovieEntry class

--- app/src/main/java/com/example/movielist/MovieEntry.java
+++ app/src/main/java/com/example/movielist/MovieEntry.java
@@ -1,4 +1,31 @@
 package com.example.movielist;
 
 public class MovieEntry {
+    private int id;
+    private String title;
+    private boolean isWatched;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isWatched() {
+        return isWatched;
+    }
+
+    public void setWatched(boolean watched) {
+        isWatched = watched;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
