1cdf2d866d981bfe5c3d5d58814324e2080d90eb
voodoopdx <ourquotesdefineus@gmail.com>
2019-03-22 10:00:58 -0700

    onClick listeners

--- app/src/main/java/com/example/watchedmovielist/EditActivity.java
+++ app/src/main/java/com/example/watchedmovielist/EditActivity.java
@@ -1,10 +1,12 @@
 package com.example.watchedmovielist;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Movie;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -16,6 +18,7 @@ public class EditActivity extends AppCompatActivity {
     private Switch watchedFlag;
     private Button saver;
     private Button ender;
+    Context context;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,16 +30,32 @@ public class EditActivity extends AppCompatActivity {
         watchedFlag = findViewById(R.id.watched_switch);
         saver = findViewById(R.id.save_bttn);
         ender = findViewById(R.id.delete_button);
+        context = this;
 
         if(movie != null) {
 
             editTextView.setText(movie.getName());
             watchedFlag.setChecked(movie.isWatched());
-
-
         }
 
-        
+        saver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewDetailIntent = new Intent(context, MainActivity.class);
+                movie = new MovieListing(editTextView.getText().toString(), watchedFlag.isChecked());
+                viewDetailIntent.putExtra("KEY", movie);
+                startActivity(viewDetailIntent);
+            }
+
+        });
+        ender.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewDetailIntent = new Intent(context, MainActivity.class);
+                
+            }
+        }));
+
         // 3. Set save/delete functionality via onclick listeners for buttons
 
 
