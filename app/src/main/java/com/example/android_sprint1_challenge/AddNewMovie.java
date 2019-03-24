9c7e553d92d24d066db2981409022155ba8eaef0
Kyle Cole <kyle@misfitselectronics.com>
2019-03-24 16:46:02 -0400

    complete rebuild fully functional (whole issue was that autoimport was on so everything that I had referenced as "Movie" was pulling up the built in Movie commands. All fixed now and still polishing for the fun of it.

--- app/src/main/java/com/example/android_sprint1_challenge/AddNewMovie.java
+++ app/src/main/java/com/example/android_sprint1_challenge/AddNewMovie.java
@@ -1,5 +1,6 @@
 package com.example.android_sprint1_challenge;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Movie;
@@ -16,69 +17,84 @@ import android.widget.TextView;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AddNewMovie extends AppCompatActivity implements Serializable {
-    private EditText movieInput;
-    private Button saveButton, deleteButton;
-    private Switch viewedSwitch;
-    //private Boolean switchViewed;
-    Context context;
-    private MovieEntry entry;
-    private String entryString;
-    private ArrayList<MovieEntry> entryList ;
-    Boolean switchViewed;
-    int id;
+    private MovieEntry movie;
+    private EditText movieTitleEntry;
+    private Switch watchedSwitch;
+    private Button saveButton;
+    private Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_movie);
-        viewedSwitch = findViewById(R.id.watched_switch);
-        movieInput = findViewById(R.id.movie_edit_text);
+
+        movieTitleEntry = findViewById(R.id.movie_edit_text);
+        watchedSwitch = findViewById(R.id.watched_switch);
         saveButton = findViewById(R.id.button_save);
         deleteButton = findViewById(R.id.button_delete);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-         Intent passList;
-        context = this;
-        passList = getIntent();
-            if (passList.getStringExtra("textmovieName") != null) {
-                String movieName = passList.getStringExtra("textmovieName");
-                id = passList.getIntExtra("textmovieId", 0);
-                MovieRepo.allMovies.remove(id);
-                MovieEntry editMovie = MovieRepo.allMovies.get(id);
 
-                //                switchViewed = passList.getBooleanExtra("textmovieBoolean",false);
-                viewedSwitch.setChecked(editMovie.getViewed());
-                entryString = editMovie.getMovieName();
-                movieInput.setText(entryString);        }
+        Intent intent = getIntent();
+        if (intent.hasExtra(MovieEntry.MOVIE_TAG)) {
+            movie = (MovieEntry) intent.getSerializableExtra(MovieEntry.MOVIE_TAG);
+            movieTitleEntry.setText(movie.getMovieName());
+            watchedSwitch.setChecked(movie.isWatched());
+        } else {
+            movie = new MovieEntry();
+        }
 
+        movieTitleEntry.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String name = s.toString();
+                movie.setMovieName(name);
+            }
+        });
 
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent saveIntent = new Intent(context, MovieListActivity.class);
-                entryString = movieInput.getText().toString();
-                MovieEntry addMovie = new MovieEntry(entryString, viewedSwitch.isChecked());
-                MovieRepo.allMovies.add(addMovie);
-                startActivity(saveIntent);
+                onSavePressed(movie);
             }
         });
+
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent deleteIntent = new Intent(context, MovieListActivity.class);
-                MovieRepo.allMovies.remove(id);
-                startActivity(deleteIntent);
+                Intent saveIntent = new Intent();
+                saveIntent.putExtra(MovieEntry.MOVIE_TAG,  movie);
+                setResult(Activity.RESULT_CANCELED, saveIntent);
+                finish();
             }
         });
+    }
 
-
-
+    protected void onSavePressed(MovieEntry movie) {
+        if (movie != null) {
+            movie.setWatched(watchedSwitch.isChecked());
+            Intent saveIntent = new Intent();
+            saveIntent.putExtra(MovieEntry.MOVIE_TAG,  movie);
+            setResult(Activity.RESULT_OK, saveIntent);
+            finish();
+        } else { onBackPressed(); }
     }
 }
