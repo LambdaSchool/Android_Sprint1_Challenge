cde5d92749858409b0af1338937437a93f425afc
Janiece Thomas <janiece.t90@gmail.com>
2019-03-29 12:12:07 -0400

    created construction and getters and setter for my movie entry

--- app/src/main/java/com/example/android_sprint1_challenge/MovieEntry.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MovieEntry.java
@@ -1,4 +1,23 @@
 package com.example.android_sprint1_challenge;
 
-public class MovieEntry {
+import java.io.Serializable;
+
+public class MovieEntry implements Serializable {
+    public static final String TAG = "movieEntry";
+
+    public static final int INVALID_ID = -1;
+
+    private String movieText;
+
+    public MovieEntry(String movieText) {
+        this.movieText = movieText;
+    }
+
+    public String getMovieText() {
+        return movieText;
+    }
+
+    public void setMovieText(String movieText) {
+        this.movieText = movieText;
+    }
 }
