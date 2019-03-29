c86959c311e9c0ccde3a78d41d6309ae9d6a0737
Janiece Thomas <janiece.t90@gmail.com>
2019-03-29 12:09:11 -0400

    added life Cycle and MovieEntry Class

--- app/src/main/java/com/example/android_sprint1_challenge/ButtonsPage.java
+++ app/src/main/java/com/example/android_sprint1_challenge/ButtonsPage.java
@@ -1,7 +1,10 @@
 package com.example.android_sprint1_challenge;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
+import android.util.Log;
 
 public class ButtonsPage extends AppCompatActivity {
 
@@ -10,4 +13,32 @@ public class ButtonsPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buttons_page);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
 }
--- app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
+++ app/src/main/java/com/example/android_sprint1_challenge/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.android_sprint1_challenge;
 
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addMovieButton;
+    Context context;
+    private ConstraintLayout movieList;
+    private ArrayList<> movieEntries;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 
 
