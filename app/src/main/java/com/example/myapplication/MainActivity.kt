5d775ef1fb750505e3f9e8a82c6b7f34a8a906e7
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-09 10:48:09 -0600

    First Intent
    First successful Intent to pass to next screen
    
    Co-Authored-By: Basil <heiligbasil@users.noreply.github.com>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val NEW_ENTRY_REQUEST = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_add_movie.setOnClickListener {
+            val nextScreenIntent = Intent (this, NextActivity::class.java)
+            startActivityForResult(nextScreenIntent, NEW_ENTRY_REQUEST)
+        }
+
+
     }
 }
