24e50c0f9244c164456ceb647afefe21ab59b90d
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-09 12:42:49 -0600

    Passing Data Intent
    this is Kotlin intent to pass data between the screens
    
    Co-Authored-By: Basil <heiligbasil@users.noreply.github.com>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,22 +3,33 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_next.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val NEW_ENTRY_REQUEST = 1
+
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val intent = intent
+        val movie = intent.getStringExtra("Name")
+        val finalView = findViewById<TextView>(R.id.final_view)
+
+        finalView.text = "Movie: $movie"
+
+
+
         button_add_movie.setOnClickListener {
             val nextScreenIntent = Intent (this, NextActivity::class.java)
             startActivityForResult(nextScreenIntent, NEW_ENTRY_REQUEST)
         }
-
 
     }
 }
--- app/src/main/java/com/example/myapplication/NextActivity.kt
+++ app/src/main/java/com/example/myapplication/NextActivity.kt
@@ -1,12 +1,32 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_next.*
 
 class NextActivity : AppCompatActivity() {
+    companion object {
+        const val ENTRY_REQUEST = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
+
+        button_save.setOnClickListener {
+            val movie = edit_entry.text.toString()
+
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("Name", movie)
+
+            startActivity(intent)
+        }
+
+
     }
+
 }
